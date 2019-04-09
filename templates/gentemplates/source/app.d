bce3b988bb78579f9a10b7313c3919221c50402a
Jedzia <jed69@gmx.de>
2019-04-10 01:04:05 +0200

    scan directories

--- templates/gentemplates/source/app.d
+++ templates/gentemplates/source/app.d
@@ -1,6 +1,42 @@
 import std.stdio;
+import std.getopt;
+import std.algorithm;
 
-void main()
-{
-	writeln("Edit source/app.d to start your project.");
+import std.file;
+
+string data = "file.dat";
+int length = 24;
+bool verbose;
+
+enum Color {
+    no,
+    yes
+}
+
+Color color;
+
+void main(string[] args) {
+    writeln("Edit source/app.d to start your project.");
+
+    auto helpInformation = getopt(args, "length", &length, // numeric
+            "file", &data, // string
+            "verbose", &verbose, // flag
+            "color", "Information about this color", &color); // enum
+
+    if (helpInformation.helpWanted) {
+        defaultGetoptPrinter("Some information about the program.", helpInformation.options);
+    }
+
+    auto pArgs = remove(args, 0);
+    //writeln("rem:", aa);
+
+    foreach (dirArg; pArgs) {
+        writeln("arg:", dirArg);
+
+        //auto txtFiles = dirEntries("../snippets", "*.*", SpanMode.breadth);
+        //auto txtFiles = dirEntries("../snippets", "*", SpanMode.shallow);
+        auto txtFiles = dirEntries(dirArg, "*", SpanMode.shallow);
+        foreach (txtFile; txtFiles)
+            writeln(txtFile, " files found..");
+    }
 }
