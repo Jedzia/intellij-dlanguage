3be62819610086f985230bf36dabf6e201e0480b
Jedzia <jed69@gmx.de>
2019-04-10 01:50:16 +0200

    prepared unit tests

--- templates/gentemplates/source/app.d
+++ templates/gentemplates/source/app.d
@@ -1,8 +1,12 @@
-import std.stdio;
-import std.getopt;
 import std.algorithm;
-
 import std.file;
+import std.getopt;
+import std.stdio;
+import std.string;
+import std.xml;
+import std.exception;
+
+//import bla;
 
 string data = "file.dat";
 int length = 24;
@@ -15,7 +19,19 @@ enum Color {
 
 Color color;
 
+/*string ordinal(size_t number) {
+    return ""; // ← intentionally wrong
+}
+
+unittest {
+    assert(ordinal(1) == "1st");
+    assert(ordinal(2) == "2nd");
+    assert(ordinal(3) == "3rd");
+    assert(ordinal(10) == "10th");
+}*/
+
 void main(string[] args) {
+    //bla b;
     writeln("Edit source/app.d to start your project.");
 
     auto helpInformation = getopt(args, "length", &length, // numeric
@@ -35,8 +51,33 @@ void main(string[] args) {
 
         //auto txtFiles = dirEntries("../snippets", "*.*", SpanMode.breadth);
         //auto txtFiles = dirEntries("../snippets", "*", SpanMode.shallow);
-        auto txtFiles = dirEntries(dirArg, "*", SpanMode.shallow);
-        foreach (txtFile; txtFiles)
-            writeln(txtFile, " files found..");
+        auto directories = dirEntries(dirArg, "*", SpanMode.shallow);
+        foreach (langDir; directories) {
+            writeln("snippet dir:", langDir);
+            auto files = dirEntries(langDir, "*.snippet", SpanMode.breadth);
+            foreach (file; files)
+                writeln(file, " files found..");
+        }
     }
+
+    writeln();
+    writeln();
+
+    auto doc = new Document(new Tag("catalog"));
+    auto element = new Element("book");
+    element.tag.attr["id"] = "123";
+    element ~= new Element("author", "Jedzia");
+
+    doc ~= element;
+
+    writefln(join(doc.pretty(3), "\n"));
+
+}
+
+unittest {
+    // generate and reparse, compare
+    //bla b;
+    //assertThrown(average([1], [1, 2]));
+    assert(42 == 42, "Shurely, 42 is not 43!");
+    //assert(true);
 }
---
+++ templates/gentemplates/source/bla.d
@@ -0,0 +1,26 @@
+module bla;
+
+import std.stdio;
+
+public class bla {
+
+    this() {
+    }
+
+    ~this() {
+    }
+
+    /*override size_t toHash() {
+        // todo
+    }*/
+
+    /*override string toString() {
+        // todo: make a useful toString()
+    }*/
+
+}
+
+public unittest {
+    //assert(42 == 43, "HA !");
+    writeln(" unittesting.. bla.d");
+}
